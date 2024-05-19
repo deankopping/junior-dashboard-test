e9c1ab97f9b28755d6aa4af0bed8da7a81586049
Dean Kopping <dkopping9@gmail.com>
2024-05-19 14:20:11 +0200

    Fixed positioning of category menu

--- src/components/CardFive.tsx
+++ src/components/CardFive.tsx
@@ -100,111 +100,117 @@ const CardFive = () => {
     cost: number;
   }) {
     return (
-      <div className="grid grid-cols-3 border-b border-stroke dark:border-strokedark sm:grid-cols-5">
-        <div className="flex items-center gap-3 p-2.5 xl:p-5">
-          <p className="hidden text-black dark:text-white sm:block">{id}</p>
+      <div className="sm-text-xs grid grid-cols-4 border-b border-stroke dark:border-strokedark">
+        <div className="flex items-center justify-center p-2.5 xl:p-5">
+          <p className="sm-text-xs text-black dark:text-white ">{id}</p>
         </div>
 
         <div className="flex items-center justify-center p-2.5 xl:p-5">
-          <p className="text-black dark:text-white">{name}</p>
+          <p className="sm-text-xs text-black dark:text-white">{name}</p>
         </div>
 
         <div className="flex items-center justify-center p-2.5 xl:p-5">
-          <p className="text-meta-3">{revenue}</p>
+          <p className="sm-text-xs text-black dark:text-white">{revenue}</p>
         </div>
 
-        <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-          <p className="text-black dark:text-white">{cost}</p>
+        <div className="flex items-center justify-center p-2.5 xl:p-5">
+          <p className="sm-text-xs text-black dark:text-white">{cost}</p>
         </div>
       </div>
     );
   }
 
   return (
-    <div className=" rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
+    <div className="relative rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
       {/**drop down menu */}
-      <button
-        id="dropdownHoverButton"
-        onMouseEnter={() => setDropdownOpen(true)}
-        onMouseLeave={() => setDropdownOpen(false)}
-        className="bg-gray text-black text-absolute focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
-        type="button"
-      >
-        {currentCategoryName}
-        <svg
-          className="w-2.5 h-2.5 ml-2.5"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 10 6"
-        >
-          <path
-            stroke="black"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="m1 1 4 4 4-4"
-          />
-        </svg>
-        {isDropdownOpen && (
-          <div
-            id="dropdownHover"
-            className="absolute z-10 mt-2 bg-gray divide-y divide-gray-100 rounded-lg shadow w-44"
-            onMouseEnter={() => setDropdownOpen(true)}
-            onMouseLeave={() => setDropdownOpen(false)}
-          >
-            <ul>
-              {cats.map((category, key) => (
-                <li
-                  key={category.id}
-                  value={category.name}
-                  className="block px-4 py-2 hover:bg-white text-black rounded-lg"
-                  onClick={() => handleCategorySelect(category)}
-                >
-                  {category.name}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </button>
+
       {/**table */}
 
-      <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
-        Products
-      </h4>
+      <span className="flex items-start">
+        <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
+          Products
+        </h4>
+        <button
+          id="dropdownHoverButton"
+          onMouseEnter={() => setDropdownOpen(true)}
+          onMouseLeave={() => setDropdownOpen(false)}
+          className="absolute right-7.5 bg-gray text-black text-absolute font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
+          type="button"
+        >
+          {currentCategoryName}
+          <svg
+            className="w-2.5 h-2.5 ml-2.5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 10 6"
+          >
+            <path
+              stroke="black"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m1 1 4 4 4-4"
+            />
+          </svg>
+          {isDropdownOpen && (
+            <div
+              id="dropdownHover"
+              className="absolute z-10 mt-2 bg-gray divide-y divide-gray-100 rounded-lg shadow w-44"
+              onMouseEnter={() => setDropdownOpen(true)}
+              onMouseLeave={() => setDropdownOpen(false)}
+            >
+              <ul>
+                {cats.map((category, key) => (
+                  <li
+                    key={category.id}
+                    value={category.name}
+                    className="block px-4 py-2 hover:bg-white text-black rounded-lg"
+                    onClick={() => handleCategorySelect(category)}
+                  >
+                    {category.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </button>
+      </span>
+      <div>
+        <div className="flex flex-col">
+          <div className="grid grid-cols-4 rounded-sm bg-gray-2 dark:bg-meta-4">
+            <div className="p-2.5 text-center sm:block xl:p-5">
+              <h5 className="text-sm font-medium uppercase xsm:text-base">
+                ID
+              </h5>
+            </div>
+            <div className="p-2.5 text-center sm:block xl:p-5">
+              <h5 className="text-sm font-medium uppercase xsm:text-base">
+                Name
+              </h5>
+            </div>
+            <div className="p-2.5 text-center sm:block xl:p-5 ">
+              <h5 className="text-sm font-medium uppercase xsm:text-base">
+                Revenue
+              </h5>
+            </div>
+            <div className="p-2.5 text-center sm:block xl:p-5">
+              <h5 className="text-sm font-medium uppercase xsm:text-base">
+                Cost
+              </h5>
+            </div>
+          </div>
 
-      <div className="flex flex-col">
-        <div className="grid grid-cols-3 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-5">
-          <div className="p-2.5 xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">ID</h5>
-          </div>
-          <div className="p-2.5 text-center xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Name
-            </h5>
-          </div>
-          <div className="p-2.5 text-center xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Revenue
-            </h5>
-          </div>
-          <div className="hidden p-2.5 text-center sm:block xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Cost
-            </h5>
-          </div>
+          {selectedCategory.map((item, key) => (
+            <TableRow
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              revenue={item.salesRevenue}
+              cost={item.cost}
+            ></TableRow>
+          ))}
         </div>
-
-        {selectedCategory.map((item, key) => (
-          <TableRow
-            key={item.id}
-            id={item.id}
-            name={item.name}
-            revenue={item.salesRevenue}
-            cost={item.cost}
-          ></TableRow>
-        ))}
       </div>
     </div>
   );
